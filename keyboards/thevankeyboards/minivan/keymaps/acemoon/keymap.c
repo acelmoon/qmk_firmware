5c53daf638fffb3485dfda3858ea185b7f2f1aff
Ace A <acemoon_da@outlook.com>
2019-10-28 22:25:32 +0000

    Update to the minivan layout

--- keyboards/thevankeyboards/minivan/keymaps/acemoon/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/acemoon/keymap.c
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		TAB_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
 		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    QUO_LT3, ENT_LT3,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, SFT_DOT, SFT_SLS, MO(1),
-		ESC_LT1, MO(2),   _______, KC_LALT,      KC_SPC,          MO(6),        SLS_LT2, _______, MO(2),   DEL_LT1),
-	  //ESC_LT1, MO(2),   MO(2),   KC_LALT,      KC_SPC,          MO(6),        SLS_LT2, SLS_LT2, MO(2),   LT1_DEL),
+		ESC_LT1, MO(2),   KC_LALT, KC_LALT,      KC_SPC,          MO(6),        SLS_LT2, _______, MO(2),   DEL_LT1),
+	  //ESC_LT1, MO(2),   _______, KC_LALT,      KC_SPC,          MO(6),        SLS_LT2, _______, MO(2),   DEL_LT1),
 
 	//1: Number & Extra Keys
 	[_FN1] = LAYOUT_arrow_command(
