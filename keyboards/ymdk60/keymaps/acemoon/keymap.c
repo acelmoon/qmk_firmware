6c37e0f797e86c173c663ab1c63f7b6c3368a3ad
Ace A <acemoon_da@outlook.com>
2019-11-18 22:48:37 +0000

    Update to YMDK60 Keymap: wrong keycode for tapdance shift_slash

--- keyboards/ymdk60/keymaps/acemoon/keymap.c
+++ keyboards/ymdk60/keymaps/acemoon/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SCN_LT3, QUO_LT3, KC_NO,            KC_ENT,//TD(QUOT_LAYER)
-      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, SFT_DOT, SFT_SLH, KC_NO,   KC_RSFT, KC_NO,
+      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, SFT_DOT, SFT_SLS, KC_NO,   KC_RSFT, KC_NO,
       ESC_LT1, MO(2),   KC_LALT, KC_NO,                       KC_SPC,                  KC_NO,   SLS_LT2, _______, KC_NO,   MO(5), DEL_LT1),
 
   //1: Numbers & F-keys
