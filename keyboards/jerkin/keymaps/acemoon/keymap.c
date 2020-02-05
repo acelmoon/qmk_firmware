20c9f51b77f50198804e8ac31ae8a001033420bb
Ace A <acemoon_da@outlook.com>
2020-02-05 02:08:24 +0000

    Jerkin30 layout optimizations

--- keyboards/jerkin/keymaps/acemoon/keymap.c
+++ keyboards/jerkin/keymaps/acemoon/keymap.c
@@ -29,32 +29,32 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[0] = LAYOUT(
+	[0] = LAYOUT( //Base Layer
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_GESC, 
 		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(3,KC_QUOT), LT(3, KC_ENT), 
-		KC_LSFT, KC_Z, KC_X, KC_C, ALT_T(KC_V), KC_SPC, LT(2,KC_B), GUI_T(KC_N), KC_M, KC_COMM, SFT_DOT, SFT_SLS, DEL_LT1),//LT(1,
+		KC_LSFT, KC_Z, KC_X, KC_C, ALT_T(KC_V), KC_SPC, LT(2,KC_B), GUI_T(KC_N), KC_M, KC_COMM, SFT_DOT, LT(1,KC_SLSH), DEL_LT1),//LT(1,
 
-	[1] = LAYOUT(
+	[1] = LAYOUT( //Number & Extra Keys
 		KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, 
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, _______, 
-		_______, _______, _______, KC_APP, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______),
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, KC_SCLN, _______, 
+		_______, _______, _______, KC_APP, _______, _______, _______, _______, _______, _______, _______, SFT_SLS, _______),//KC_PSCR
 
-	[2] = LAYOUT(
+	[2] = LAYOUT( //F-Keys & Functions
 		_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
 		_______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_INS, _______, 
-		_______, _______, _______, _______, _______, KC_PASTA, _______, _______, _______, _______, _______, _______, _______),
+		MO(4), _______, _______, _______, _______, KC_PASTA, _______, _______, _______, _______, _______, _______, _______),
 
-	[3] = LAYOUT(
+	[3] = LAYOUT( //Home-Row Arrows & Extra Keys
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 	
-	[4] = LAYOUT(
+	[4] = LAYOUT( //Settings
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET),
 	
-	[5] = LAYOUT(
+	[5] = LAYOUT( //Mouse Keys
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
