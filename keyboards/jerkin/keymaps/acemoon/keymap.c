bc8e657363a07f540d6a8d87d97fb324889ce401
Ace A <acemoon_da@outlook.com>
2020-02-12 01:45:34 +0000

    Update to jerkin30 keymap. -Swapped function key placement in order to replicate usage from MM2

--- keyboards/jerkin/keymaps/acemoon/keymap.c
+++ keyboards/jerkin/keymaps/acemoon/keymap.c
@@ -34,12 +34,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT( //Base Layer
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, GUI_T(KC_ESC), //KC_GESC
 		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(3,KC_QUOT), LT(3, KC_ENT), 
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_SPC, LT(2,KC_B), KC_N, KC_M, KC_COMM, SFT_DOT, SFT_SLS, DEL_LT1),//LT(1,  LT(1,KC_SLSH)
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_SPC, LT(2,KC_B), KC_N, KC_M, KC_COMM, SFT_DOT, LT(1,KC_SLSH), DEL_LT1),//LT(1,   SFT_SLS
 
 	[1] = LAYOUT( //Number & Extra Keys
 		KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, _______, 
 		_______, _______, _______, _______, _______, _______, KC_NUBS, KC_NUHS, KC_GRV, KC_SCLN, KC_SCLN, KC_ENT, 
-		_______, _______, _______, _______, KC_APP, _______, KC_BSPC, _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_RSFT, KC_DEL),//KC_PSCR
+		_______, _______, _______, _______, KC_APP, _______, KC_BSPC, _______, _______, _______, KC_RSFT, _______, KC_DEL),//KC_PSCR
 
 	[2] = LAYOUT( //F-Keys & Functions
 		KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, _______, 
